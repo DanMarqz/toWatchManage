9d7f5bd2eaa19a7930d8e94a831542e89e5ee874
Daniel <danielarturo.marquez@gmail.com>
2021-01-12 13:24:08 -0400

    Agregado de dependencias

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Agenda de contactos web</title>
+  <link rel="icon" type="image/png" href="public/img/favicon.png">
+  <?php require_once "dependencias.php"; ?>
+</head>
+<body>
+  
+</body>
+</html>
