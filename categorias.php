9e50618e076e82d3ed0ed0975acb2c019a8fbc37
Daniel <danielarturo.marquez@gmail.com>
2021-01-14 22:34:50 -0400

    Making menu

---
+++ categorias.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Pets Pals - Categorías</title>
+  <link rel="icon" type="image/png" href="public/img/favicon.png">
+  <?php require_once "dependencias.php"; ?>
+</head>
+<body>
+  <div class="container">
+    <?php require_once "menu.php";?>
+  </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-  <title>Agenda de contactos web</title>
+  <title>Pets Pals - Inicio</title>
   <link rel="icon" type="image/png" href="public/img/favicon.png">
   <?php require_once "dependencias.php"; ?>
 </head>
 <body>
-  
+  <div class="container">
+    <?php require_once "menu.php";?>
+  </div>
 </body>
 </html>
