ac6e4e069f240aabcb19aa2b6c60675c5a62d3b0
Daniel Marquez <6404667-DanMarqz@users.noreply.gitlab.com>
2021-01-20 21:39:47 -0400

    link act

--- dependencias.php
+++ dependencias.php
@@ -1,13 +1,21 @@
 
 
-  <link rel="stylesheet" href="./public/bootstrap/bootstrap.min.css">
-  <link rel="stylesheet" href="./public/fontawesome/css/all.css">
-  <link rel="stylesheet" href="./public/datatable/bootstrap.css">
-  <link rel="stylesheet" href="./public/datatable/dataTables.bootstrap4.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="./public/fontawesome/css/all.css"> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
 
-  <script src="./public/jquery.min.js"></script>
-  <script src="./public/bootstrap/popper.min.js"></script>
-  <script src="./public/bootstrap/bootstrap.min.js"></script>
-  <script src="./public/datatble/jquery.dataTables.min.js"></script>
-  <script src="./public/datatble/dataTables.bootstrap4.min.js"></script>
-  <script src="./public/sweetalert.min.js"></script>
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <!-- pooper y bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" 
+  crossorigin="anonymous"></script>
+
+  <!-- datatable -->
+  <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+
+  <!-- sweetalert -->
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <script src="https://kit.fontawesome.com/8034da982a.js" crossorigin="anonymous"></script>
